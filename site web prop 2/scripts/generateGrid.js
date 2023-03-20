6dabf726be723fdfdd2bc57fba8438bea32fd5c2
Et-si-on-debattait <127480149+Et-si-on-debattait@users.noreply.github.com>
2023-03-20 11:08:04 +0100

    Create generateGrid.js

---
+++ site web prop 2/scripts/generateGrid.js
@@ -0,0 +1,21 @@
+Papa.parse('icons.csv', {
+  download: true,
+  header: true,
+  complete: function(results) {
+    const icons = results.data;
+    const iconGrid = document.getElementById('icon-grid');
+    icons.forEach(icon => {
+      const iconElement = document.createElement('div');
+      iconElement.className = 'icon';
+      const imgElement = document.createElement('img');
+      imgElement.src = icon.image_url;
+      imgElement.alt = icon.name;
+      iconElement.appendChild(imgElement);
+      const captionElement = document.createElement('span');
+      captionElement.className = 'caption';
+      captionElement.textContent = icon.name;
+      iconElement.appendChild(captionElement);
+      iconGrid.appendChild(iconElement);
+    });
+  }
+});
